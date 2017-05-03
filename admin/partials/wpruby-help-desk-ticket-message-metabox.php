41d823f207aaef21cea31c391ddf00c24c8d8584
Waseem Senjer <waseem.senjer@gmail.com>
2017-05-03 21:50:52 +0200

    enhancing attachments admin UI

--- admin/partials/wpruby-help-desk-ticket-message-metabox.php
+++ admin/partials/wpruby-help-desk-ticket-message-metabox.php
@@ -1,17 +1,26 @@
-<?php if(isset($ticket->post_content)): ?>
-        <p><?php echo  $ticket->post_content; ?></p>
-<?php endif; ?>
+
+<div id="ticket_message">
 
 
-<h4><?php _e('Attachments', 'wpruby-help-desk'); ?></h4>
+  <div id="ticket_content">
 
 
-<?php if ($attachments): ?>
-<div class="ticket_attachments">
-  <ul>
-  <?php foreach ($attachments as $attachment): ?>
-      <li><a target="_blank" href="<?php echo wp_get_attachment_url($attachment->ID) ?>"> <?php echo basename( wp_get_attachment_url($attachment->ID) ); ?></a></li>
-  <?php endforeach; ?>
-  </ul>
+    <?php if(isset($ticket->post_content)): ?>
+            <p><?php echo  $ticket->post_content; ?></p>
+    <?php endif; ?>
+  </div>
+
+
+    <?php if ($attachments): ?>
+      <hr>
+      <h3><?php _e('Attachments', 'wpruby-help-desk'); ?></h3>
+
+      <div class="ticket_attachments">
+        <ol>
+        <?php foreach ($attachments as $attachment): ?>
+            <li><a target="_blank" href="<?php echo wp_get_attachment_url($attachment->ID) ?>"> <?php echo basename( wp_get_attachment_url($attachment->ID) ); ?></a></li>
+        <?php endforeach; ?>
+      </ol>
+      </div>
+    <?php endif; ?>
 </div>
-<?php endif; ?>
