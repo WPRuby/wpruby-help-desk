1741973d75f13a19cf0480cba4ccfadc44465eaa
Waseem Senjer <waseem.senjer@gmail.com>
2017-06-12 21:23:12 +0200

    [1.2.0] add CF metabox file

---
+++ admin/partials/ruby-help-desk-custom-fields-metabox.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Display Custom Fields information
+ * @link       https://wpruby.com
+ * @since      1.2.0
+ *
+ * @package    Ruby_Help_Desk
+ * @subpackage Ruby_Help_Desk/admin/partials
+ */
+
+?>
+
+
+<div class="clear"></div>
